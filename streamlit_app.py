44addfb867bcad1de1f5895b087e88631d3aabe8
Daniil-S117 <108061376+Daniil-S117@users.noreply.github.com>
2024-12-17 15:38:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,23 +4,9 @@ from transformers import pipeline
 text = "Этот курс был создан в партнерстве с Hugging Face."
 
 
-def translation_ru_en(_input_text):
-    translator = pipeline("translation", model="facebook/nllb-200-distilled-600M", src_lang='rus_Cyrl',
-                          tgt_lang='eng_Latn')
-    translation = translator(_input_text)
-    return translation
-
-
-def translation_en_ru(_input_text):
-    translator = pipeline("translation", model="facebook/nllb-200-distilled-600M", src_lang='eng_Latn',
-                          tgt_lang='rus_Cyrl')
-    translation = translator(_input_text)
-    return translation
-
-
-def translation_ru_fr(_input_text):
-    translator = pipeline("translation", model="facebook/nllb-200-distilled-600M", src_lang='rus_Cyrl',
-                          tgt_lang='fra_Latn')
+def translate(_input_text, _src_lang, _tgt_lang):
+    translator = pipeline("translation", model="facebook/nllb-200-distilled-600M",
+                          src_lang=_src_lang, tgt_lang=_tgt_lang)
     translation = translator(_input_text)
     return translation
 
@@ -33,19 +19,17 @@ selected_task = st.sidebar.selectbox("Выбрать язык: ",
 input_text = st.text_area("Введите текст:")
 
 if st.button("Перевести"):
+    st.subheader("Перевод:")
     if selected_task == "Русский на Английский" and input_text:
-        st.subheader("Перевод:")
-        result = translation_ru_en(input_text)
+        result = translate(input_text, 'rus_Cyrl', 'eng_Latn')
         st.write('', str(result).strip("[{'translation_text': ']}\'"))
+        st.write(str(result))
     elif selected_task == "Английский на Русский" and input_text:
-        st.subheader("Перевод")
-        result = translation_en_ru(input_text)
+        result = translate(input_text, 'eng_Latn', 'rus_Cyrl')
         st.write('', str(result).strip("[{'translation_text': ']}\'"))
     elif selected_task == "Русский на Французкий" and input_text:
-        st.subheader("Перевод:")
-        result = translation_ru_fr(input_text)
+        result = translate(input_text, 'rus_Cyrl', 'fra_Latn')
         st.write('', str(result).strip("[{'translation_text': ']}\'"))
     else:
         st.info("Введите текст и выберите задачу на боковой панели.")
-
 
